5abca212a71faa9ff60f635fbc306168fa2ddc85
Vitaly Kovalyshyn <v.kovalyshyn@webitel.com>
2019-07-17 16:11:19 +0300

    Webitel Go image - first blod :)

--- webitel-docker-swarm.sh
+++ webitel-docker-swarm.sh
@@ -6,13 +6,13 @@ docker network create --config-only \
 	--subnet 192.168.177.0/24 \
 	--gateway 192.168.177.1 \
 	--ip-range 192.168.177.8/29 \
-	--aux-address 'host=192.168.177.9' \
+	--aux-address 'host=192.168.177.8' \
 	-o parent=$PARENTDEV wnet_macvlan
 docker network create -d macvlan --scope swarm --config-from wnet_macvlan wnet_macvlan_swarm
 docker stack deploy --compose-file webitel-portainer-stack.yml swarm
 
 ip link add wbr0 link $PARENTDEV type macvlan mode bridge
-ip addr add 192.168.177.9/32 dev wbr0
+ip addr add 192.168.177.8/32 dev wbr0
 ip link set wbr0 up
 ip route add 192.168.177.8/29 dev wbr0
 
