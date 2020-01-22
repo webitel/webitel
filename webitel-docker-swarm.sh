7ee220c092371995815dcb3113d29aa5aed356c7
Vitaly Kovalyshyn <vitaly@kovalyshyn.pp.ua>
2020-01-22 10:17:40 +0200

    update docker swarm files

--- webitel-docker-swarm.sh
+++ webitel-docker-swarm.sh
@@ -13,6 +13,8 @@ docker network create --config-only \
         -o parent=$PARENTDEV wnet_macvlan
 docker network create -d macvlan --scope swarm --config-from wnet_macvlan wnet_macvlan_swarm
 
+docker network create -d overlay --subnet=10.11.0.0/24 wtel_vlan
+
 docker stack deploy --compose-file webitel-portainer-stack.yml swarm
 
 exit 0
