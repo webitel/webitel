f90eacb65d803aa193a0b1c6dd01c97662352a35
Vitaly Kovalyshyn <vitaly@kovalyshyn.pp.ua>
2020-01-22 14:14:25 +0200

    RabbitMQ HA

--- install.sh
+++ install.sh
@@ -2,13 +2,16 @@
 export PARENTDEV=enp2s0
 
 docker stack deploy --compose-file webitel-portainer-stack.yml swarm
-
 docker network create -d overlay --subnet=10.11.0.0/24 --attachable wtel_vlan
 
+#
+#docker stack deploy --compose-file webitel-consul-stack.yml swarm
+#
 #docker node update --label-add rabbitmq1=true node-1
 #docker node update --label-add rabbitmq2=true node-2
-#docker stack deploy --compose-file webitel-consul-stack.yml swarm
 #docker stack deploy --compose-file webitel-rabbitmq-stack.yml swarm
+#https://www.rabbitmq.com/ha.html
+#
 
 ip link add wbr0 link $PARENTDEV type macvlan mode bridge
 ip addr add 192.168.177.8/32 dev wbr0
