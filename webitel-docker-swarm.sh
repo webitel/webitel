e145f6f2943638b33532227df9d3ba9df45f1242
Vitaly Kovalyshyn <vitaly@kovalyshyn.pp.ua>
2020-01-22 12:44:32 +0200

    RabbitMQ

--- webitel-docker-swarm.sh
+++ webitel-docker-swarm.sh
@@ -3,6 +3,11 @@ export PARENTDEV=enp2s0
 
 docker stack deploy --compose-file webitel-portainer-stack.yml swarm
 
+docker network create -d overlay --subnet=10.11.0.0/24 --attachable wtel_vlan
+
+docker stack deploy --compose-file webitel-consul-stack.yml swarm
+docker stack deploy --compose-file webitel-rabbitmq-stack.yml swarm
+
 ip link add wbr0 link $PARENTDEV type macvlan mode bridge
 ip addr add 192.168.177.8/32 dev wbr0
 ip link set wbr0 up
@@ -14,7 +19,5 @@ docker network create --config-only \
         --aux-address 'host=192.168.177.8' \
         -o parent=$PARENTDEV wnet_macvlan
 docker network create -d macvlan --scope swarm --config-from wnet_macvlan wnet_macvlan_swarm
-
-docker network create -d overlay --subnet=10.11.0.0/24 --attachable wtel_vlan
 
 exit 0
