9dcdd7b8d52883da77b0b0155bfe00c3d15e5628
Vitaly Kovalyshyn <vitaly@kovalyshyn.pp.ua>
2020-01-24 17:23:53 +0200

    updated consul\rabbit stacks

--- install.sh
+++ install.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
-export PARENTDEV=enp2s0
+export PARENTDEV=eno1
 
 docker stack deploy --compose-file webitel-portainer-stack.yml swarm
+
+#ip link del wbr0
+ip link add wbr0 link $PARENTDEV type macvlan mode bridge
+ip addr add 192.168.83.8/32 dev wbr0
+ip link set wbr0 up
+ip route add 192.168.83.8/29 dev wbr0
+docker network create --config-only \
+        --subnet 192.168.83.0/24 \
+        --gateway 192.168.83.1 \
+        --ip-range 192.168.83.8/29 \
+        --aux-address 'node1=192.168.83.8' \
+        -o parent=$PARENTDEV wnet_macvlan
+docker network create -d macvlan --scope swarm --config-from wnet_macvlan wnet_macvlan_swarm
+
 docker network create -d overlay --subnet=10.11.0.0/24 --attachable wtel_vlan
 
 #
@@ -13,16 +27,5 @@ docker network create -d overlay --subnet=10.11.0.0/24 --attachable wtel_vlan
 #https://www.rabbitmq.com/ha.html
 #
 
-ip link add wbr0 link $PARENTDEV type macvlan mode bridge
-ip addr add 192.168.177.8/32 dev wbr0
-ip link set wbr0 up
-ip route add 192.168.177.8/29 dev wbr0
-docker network create --config-only \
-        --subnet 192.168.177.0/24 \
-        --gateway 192.168.177.1 \
-        --ip-range 192.168.177.8/29 \
-        --aux-address 'host=192.168.177.8' \
-        -o parent=$PARENTDEV wnet_macvlan
-docker network create -d macvlan --scope swarm --config-from wnet_macvlan wnet_macvlan_swarm
 
 exit 0
