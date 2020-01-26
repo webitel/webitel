b84435f5ed3ef18a5618bd6977d0da10e9ae50f3
Vitaly Kovalyshyn <vitaly@kovalyshyn.pp.ua>
2020-01-26 18:50:10 +0200

    FreeSWITCH config files

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # Webitel Stack
 
+## portainer
+
+    docker stack deploy --compose-file webitel-portainer-stack.yml swarm
+
+## consul
+
+    docker stack deploy --compose-file webitel-consul-stack.yml swarm
+
+## rabbitmq
+
+    docker node update --label-add rabbitmq1=true node-1
+    docker node update --label-add rabbitmq2=true node-2
+    docker stack deploy --compose-file webitel-rabbitmq-stack.yml swarm
+
+HA: https://www.rabbitmq.com/ha.html
+
--- mkvxlan.sh
+++ mkvxlan.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 export PARENTDEV=eno1
 
-docker stack deploy --compose-file webitel-portainer-stack.yml swarm
+ip link del wbr0
+docker network rm wtel_vlan wnet_macvlan_swarm wnet_macvlan
 
-#ip link del wbr0
 ip link add wbr0 link $PARENTDEV type macvlan mode bridge
 ip addr add 192.168.83.8/32 dev wbr0
 ip link set wbr0 up
 ip route add 192.168.83.8/29 dev wbr0
+
 docker network create --config-only \
         --subnet 192.168.83.0/24 \
         --gateway 192.168.83.1 \
@@ -15,17 +16,6 @@ docker network create --config-only \
         --aux-address 'node1=192.168.83.8' \
         -o parent=$PARENTDEV wnet_macvlan
 docker network create -d macvlan --scope swarm --config-from wnet_macvlan wnet_macvlan_swarm
-
 docker network create -d overlay --subnet=10.11.0.0/24 --attachable wtel_vlan
-
-#
-#docker stack deploy --compose-file webitel-consul-stack.yml swarm
-#
-#docker node update --label-add rabbitmq1=true node-1
-#docker node update --label-add rabbitmq2=true node-2
-#docker stack deploy --compose-file webitel-rabbitmq-stack.yml swarm
-#https://www.rabbitmq.com/ha.html
-#
-
 
 exit 0
