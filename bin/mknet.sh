5e383904af164274e362d99405ef8f372e5534cf
Vitaly Kovalyshyn <vitaly@kovalyshyn.pp.ua>
2020-01-31 09:16:50 +0200

    refactoring consul stack file

--- bin/mknet.sh
+++ bin/mknet.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 docker network create -d macvlan --scope swarm --config-from wnet_macvlan wnet_macvlan_swarm
-docker network create -d overlay --subnet=10.11.0.0/24 --attachable wtel_vlan
+docker network create -d overlay --attachable wtel_vlan
 
 exit 0
